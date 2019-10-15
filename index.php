1c61adbb33d2bb6752ca0e62e9f8ac0d5bc51c07
kanoe <noe.ngongo20@gmail.com>
2019-10-15 21:23:09 +0200

    index

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php include('header.php')?>
+<?php include('header.php');
+
+?>
 
     <!-- Hero Section-->
       <div class="container">
@@ -7,6 +9,7 @@
               <table class="table condensed"> 
                 <thead>  
                     <tr> 
+                        <th>Id </th>
                         <th>Titre </th>
                         <th> Auteur</th>
                         <th> apercu </th>
@@ -14,19 +17,34 @@
                     </tr>
                 </thead>
                 <?php 
-                    $result = $mysqli->query("SELECT  * FROM article ");
+                  $offset = 3;
+                  $beginPage = 1;
+                    //var_dump($_GET);
+                    if(isset($_GET['beginPage'])) {
+                      $beginPage = $_GET['beginPage'] * 3 ;
+                    }
 
-                    while ($data = $result->fetch_assoc()) { ?>
+
+                    $result = $mysqli->query(" select * from article LIMIT $beginPage,".$offset);
+                    $data = $result->fetch_all(MYSQLI_ASSOC);
+                    
+                    for ( $i = 0 ; $i < count($data); $i++ ) { ?>
                         <tr>
-                            <td> <?php echo $data['titre']; ?> </td>
-                            <td> <?php echo $data['nom'].' '.$data['prenom']; ?> </td>
-                            <td class="textToLong"> <?php echo $data['contenu']; ?> </td>
-                            <td> <?php echo "<a href='view.php?id=".$data['id']."'>voir </a>" ?> </td>
+                        <td> <?php echo $data[$i]['id']; ?> </td>
+                            <td> <?php echo $data[$i]['titre']; ?> </td>
+                            <td> <?php echo $data[$i]['nom'].' '.$data[$i]['prenom']; ?> </td>
+                            <td class="textToLong"> <?php echo $data[$i]['contenu']; ?> </td>
+                            <td> <?php echo "<a href='view.php?id=".$data[$i]['id']."'>voir </a>" ?> </td>
+                            <td> <?php echo "<a href='modify.php?id=".$data[$i]['id']."'>modifier</a>" ?> </td>
+                            <td> <?php echo "<a href='supprimer.php?id=".$data[$i]['id']."'>supprimer</a>" ?> </td>
                         </tr>
                    <?php }
                    $result->free();
                     ?>
               </table>
+              <a href='?offset=1'>  1 </a>
+              <a href='?offset=2'>  2 </a>
+              <a href='?offset=3'>  3 </a>
             </div>
         </div>
   </div>
