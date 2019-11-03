b77c6ec5ab057cf2d449bce0863bead5cebeedbc
kanoe <noe.ngongo20@gmail.com>
2019-11-03 23:45:51 +0100

    refactor

--- header.php
+++ header.php
@@ -50,8 +50,8 @@
         <div class="container">
           <!-- Navbar Brand -->
           <div class="navbar-header d-flex align-items-center justify-content-between">
-            <!-- Navbar Brand --><li  class="navbar-brand">Muzik Kontrol</li>
-            <!-- Toggle Button-->
+            <!-- Navbar Brand --><a href="index.php"  class="navbar-brand">Muzik Kontrol</a>
+            <!-- Toggle Button--> 
             <button type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span></span><span></span><span></span></button>
           </div>
           <!-- Navbar Menu -->
