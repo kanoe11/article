40969543fc28a43f6c3fdcdd0d2f6ee06d2f4857
kanoe <noe.ngongo20@gmail.com>
2019-11-14 16:05:44 +0100

    add tables, user ,type

--- blog.sql
+++ blog.sql
@@ -2,11 +2,24 @@ CREATE DATABASE blog;
 
 CREATE TABLE article
 (
-id int(11)  NOT NULL PRIMARY KEY AUTO_INCREMENT,
-titre varchar(100),
-nom   varchar(100),
-prenom varchar(100),
-contenu text,
+    id int(20)  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titre varchar(100) NOT NULL ,
+    user_id int(50) NOT NULL REFERENCES uti 
+    contenu text NOT NULL,
+    type_article_id int NOT NULL,
+    FOREIGN key (type_article_id) REFERENCES type_article (id);
 );
+
+create table user ( 
+    id int(20) not null primary key auto_increment, 
+    prenom varchar(100) not null , 
+    nom varchar(100) not null , 
+    age int not null
+    );
+
+create table type_article ( 
+    id int not null , 
+    nom varchar(100) not null
+    );
 
 
