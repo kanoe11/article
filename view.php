252471c13d394466f98cd7c9ee36ebe192766c83
kanoe <noe.ngongo20@gmail.com>
2019-11-18 22:52:40 +0100

    essay fix

--- view.php
+++ view.php
@@ -2,16 +2,16 @@
 require 'config.php';
 if(isset($_GET['voir']) && !empty($_GET['voir'])) {
 	$id = $_GET['voir'];
-	$result = $mysqli->query(" select * from article where id = ".$id);
+	$result = $mysqli->query(" select * from article ");
     $data = $result->fetch_all(MYSQLI_ASSOC);
 }
 ?>
 
     <div class="container">
       <div class="row">
-            <h1><?php echo $data[0]['titre']?></h1>
+            <h1><?php echo $data[$i]['titre']; ?></h1>
             <div class="post-body  mt-5">
-                  <p class="lead"> <?php echo $data[0]['prenom'] ?><?php echo $data[0]['nom']?></p>
+                  <p class="lead"><?php echo $data[$i]['nom'].' '.$data[$i]['prenom']; ?></p>
                   <p><?php echo $data[0]['contenu'] ?>
             </div>
         </div>
