2a0affe183689fa337fcd6d212192529a48bf609
kanoe <noe.ngongo20@gmail.com>
2019-11-21 22:18:11 +0100

    add jquery

--- header.php
+++ header.php
@@ -24,9 +24,7 @@
     <link rel="stylesheet" href="css/custom.css">
     <!-- Favicon-->
     <link rel="shortcut icon" href="favicon.png">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    <script src="vendor/jquery/jquery.min.js"></script>
   </head>
   <body>
         <header class="header">
